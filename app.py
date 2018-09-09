f18a684a6e97bcdac5c9d43956bba1f42fb36f2b
sigmarola <42818933+sigmarola@users.noreply.github.com>
2018-09-09 20:50:09 +0000

    Update app.py

--- app.py
+++ app.py
@@ -8,19 +8,33 @@ from bottle import default_app, request, route, response, get
 from sys import argv
 
 bottle.debug(True)
+main = Bottle()
 
-
-@get('/<name>')
-def index(name):
-    if name == 'home':
+@route('/')
+def redir():
+    redirect('/path?id=1')
+@route('/path')
+def index():
+    id = request.query.id
+    if  id == '1':
         return '<h1>Home</h1>' \
-               '<a href="/home">Home</a><br><a href="/about">About</a><br><a href="/contact">Contact</a>'
-    elif name == 'about':
+               '<link rel="stylesheet" href="static/css/style.css">' \
+               '<a href="/path?id=1"><img src="static/img/shib1.jpg"></a><br><a href="/path?id=2"><img src="static/img/shib2.jpg"></a><br><a href="/path?id=3"><img src="static/img/shib3.jpg"></a>'
+    elif id == '2':
         return '<h1>About</h1>' \
-               '<a href="/home">Home</a><br><a href="/about">About</a><br><a href="/contact">Contact</a>'
-    elif name == 'contact':
+               '<link rel="stylesheet" href="static/css/style.css">' \
+               '<a href="/path?id=1"><img src="static/img/shib2.jpg"></a><br><a href="/path?id=2"><img src="static/img/shib1.jpg"></a><br><a href="/path?id=3"><img src="static/img/shib3.jpg"></a>'
+    elif id == '3':
         return '<h1>Contact</h1>' \
-               '<a href="/home">Home</a><br><a href="/about">About</a><br><a href="/contact">Contact</a>'
-
+               '<link rel="stylesheet" href="static/css/style.css">' \
+               '<a href="/path?id=1"><img src="static/img/shib3.jpg"></a><br><a href="/path?id=1"><img src="static/img/shib1.jpg"></a><br><a href="/path?id=3"><img src="static/img/shib2.jpg"></a>'
+    else:
+        abort(404)
+@Bottle.error(main, 404)
+def error404(error):
+    return '<h1>villa</h1>'
+@route('/static/<skra:path>')
+def static_skrar(skra):
+    return static_file(skra, root='./public/')
 
 bottle.run(host='0.0.0.0', port=argv[1])
