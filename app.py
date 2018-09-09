b8a169df00df566a4d4c351e96ec97f77d975821
sigmarola <42818933+sigmarola@users.noreply.github.com>
2018-09-09 20:41:22 +0000

    Update app.py

--- app.py
+++ app.py
@@ -9,9 +9,7 @@ from sys import argv
 
 bottle.debug(True)
 
-@get('/')
-def redir():
-    redirect('/home')
+
 @get('/<name>')
 def index(name):
     if name == 'home':
@@ -29,31 +27,5 @@ def index(name):
 def error404(error):
     return '<h1>villa</h1>'
 
-"""@route('/')
-def redir():
-    redirect('/path?id=1')
-@route('/path')
-def index():
-    id = request.query.id
-    if  id == '1':
-        return '<h1>Home</h1>' \
-               '<link rel="stylesheet" href="static/css/style.css">' \
-               '<a href="/path?id=1"><img src="static/img/shib1.jpg"></a><br><a href="/path?id=2"><img src="static/img/shib2.jpg"></a><br><a href="/path?id=3"><img src="static/img/shib3.jpg"></a>'
-    elif id == '2':
-        return '<h1>About</h1>' \
-               '<link rel="stylesheet" href="static/css/style.css">' \
-               '<a href="/path?id=1"><img src="static/img/shib2.jpg"></a><br><a href="/path?id=2"><img src="static/img/shib1.jpg"></a><br><a href="/path?id=3"><img src="static/img/shib3.jpg"></a>'
-    elif id == '3':
-        return '<h1>Contact</h1>' \
-               '<link rel="stylesheet" href="static/css/style.css">' \
-               '<a href="/path?id=1"><img src="static/img/shib3.jpg"></a><br><a href="/path?id=1"><img src="static/img/shib1.jpg"></a><br><a href="/path?id=3"><img src="static/img/shib2.jpg"></a>'
-    else:
-        abort(404)
-@error(404)
-def error404(error):
-    return '<h1>villa</h1>'
-@route('/static/<skra:path>')
-def static_skrar(skra):
-    return static_file(skra, root='./public/')"""
 
 bottle.run(host='0.0.0.0', port=argv[1])
