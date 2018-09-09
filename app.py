7060849a12cb0f19aa93c2efde407340202b2290
sigmarola <42818933+sigmarola@users.noreply.github.com>
2018-09-09 20:10:20 +0000

    Update app.py

--- app.py
+++ app.py
@@ -9,8 +9,27 @@ from sys import argv
 
 bottle.debug(True)
 
-
 @route('/')
+def redir():
+    redirect('/home')
+@route('/<name>')
+def index(name):
+    if name == 'home':
+        return '<h1>Home</h1>' \
+               '<a href="/home">Home</a><br><a href="/about">About</a><br><a href="/contact">Contact</a>'
+    elif name == 'about':
+        return '<h1>About</h1>' \
+               '<a href="/home">Home</a><br><a href="/about">About</a><br><a href="/contact">Contact</a>'
+    elif name == 'contact':
+        return '<h1>Contact</h1>' \
+               '<a href="/home">Home</a><br><a href="/about">About</a><br><a href="/contact">Contact</a>'
+    else:
+        abort(404)
+@error(404)
+def error404(error):
+    return '<h1>villa</h1>'
+
+"""@route('/')
 def redir():
     redirect('/path?id=1')
 @route('/path')
@@ -35,6 +54,6 @@ def error404(error):
     return '<h1>villa</h1>'
 @route('/static/<skra:path>')
 def static_skrar(skra):
-    return static_file(skra, root='./public/')
+    return static_file(skra, root='./public/')"""
 
 bottle.run(host='0.0.0.0', port=argv[1])
